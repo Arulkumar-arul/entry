bcfbd870044ac8537efdae73a12e07ab4f6d32aa
Arulkumar-arul <163402868+Arulkumar-arul@users.noreply.github.com>
2025-07-02 11:59:38 +0530

    sum of javascript

---
+++ sum-jacascript/script.js
@@ -0,0 +1,23 @@
+let a=6
+let b=9
+console.log(a+b)
+let c=6
+let d=9
+console.log(c-d)
+var e=4
+var f=21
+console.log(e*f)
+var num=20
+var num2=4
+console.log(num/num2)
+
+let n=14
+function squre(n){
+    console.log(n**2);
+}
+squre(n)
+
+function cube(m){
+    console.log(m**3)
+}
+cube(15)
