665936d1478ddb087783d57f5ecc8a0e1e53ea0b
Arulkumar-arul <163402868+Arulkumar-arul@users.noreply.github.com>
2025-07-21 10:02:00 +0530

    javascript

---
+++ js-to-do list/script.js
@@ -0,0 +1,10 @@
+function addtask(){
+    const taskvalue=document.getElementById("task").value;
+    if(taskvalue.trim()===""){
+        alert("Please enter the a value")
+    }
+    const newtask=document.createElement("li");
+    newtask.innerText=taskvalue
+    document.getElementById("input").appendChild(newtask);
+    document.getElementById("task").value="";
+}
